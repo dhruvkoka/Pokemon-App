a6f3ca7b5f879276e94d0a95acc22d46312ec35d
dhruvkoka <dhrumuk@gmail.com>
2025-09-03 20:23:45 -0500

    change1

---
+++ weather.js
@@ -0,0 +1,4 @@
+let input=document.getElementById("city")
+let temp = document.getElementById("par1")
+let condition = document.getElementById("par2")
+let humidity = document.getElementById("par3")
