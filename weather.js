4f65d46e4099542c4351b5a2873882411fb8f10f
dhruvkoka <dhrumuk@gmail.com>
2025-09-03 21:15:18 -0500

    change3

--- weather.js
+++ weather.js
@@ -1,4 +1,25 @@
-let input=document.getElementById("city")
-let temp = document.getElementById("par1")
-let condition = document.getElementById("par2")
-let humidity = document.getElementById("par3")
+fetchdata()
+async function fetchdata(){
+    try{
+        const city =document.getElementById("city").value.tolowerCase()
+        const findWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={707b7261d799f9bb79ac33db43426d07}`)
+
+        if (!response.ok){
+            throw new Error("This city does not exist")
+
+
+        }
+        const data = await response.json()
+        console.log(data)
+
+
+
+    }
+    catch(error){
+        console.error(error)
+
+    }
+
+}
+
+
